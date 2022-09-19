c39aba1778bde92c7d8e2c1561fea06249bd7c76
Kelo <meetkelo@outlook.com>
2022-09-19 21:17:56 +0800

    update

---
+++ pack.py
@@ -0,0 +1,26 @@
+import re
+import os
+
+filename="solution.h"
+
+def replace(W):
+  with open(filename,'r',encoding="utf-8") as f:
+    newlines = []
+    for line in f.readlines():
+      if line.strip().startswith("double W = "):
+        line = "  double W = %f;\n" % W
+      newlines.append(line)
+  with open(filename, 'w') as f:
+    for line in newlines:
+      f.write(line)
+
+W = [1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3]
+
+for w in W:
+  replace(w)
+  os.system("zip %.2f.zip solution.h solution.cpp" % w)
+
+zipfiles = ["%.2f.zip" % w for w in W]
+
+os.system("zip all.zip %s" % " ".join(zipfiles))
+os.system("rm %s" % " ".join(zipfiles))
